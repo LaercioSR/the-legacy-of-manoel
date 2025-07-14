cc45a129611991a7634c0c7d6b7970c1a90adfd8
Laercio Rios <contact@laerciorios.com>
2025-07-14 08:25:22 -0300

    feat: add images to scenes 4, 5 & 6

--- scripts/game/scene_6.gd
+++ scripts/game/scene_6.gd
@@ -1,6 +1,11 @@
 extends Node2D
 class_name Scene6
 
+@export var hud: CanvasLayer = null
+
+var titleLabel: Label = null
+var bgTexture: TextureRect = null
+
 const DIALOG_SCREEN: PackedScene = preload("res://scenes/ui/dialogs/dialog_screen.tscn")
 var dialog_data: Dictionary = {
 	0: {
@@ -10,8 +15,29 @@ var dialog_data: Dictionary = {
 		"dialog": "Com o tempo novos moradores chegaram e a fazenda passou a ser Vila, Povoado, Distrito, até que em 09 de maio de 1985 Pintadas finalmente se tornou cidade."
 	}
 }
-
-@export var hud: CanvasLayer = null
+var phases = [
+	{
+		"bg": "res://assets/textures/scene_6_1.webp",
+		"title": "fazenda"
+	},
+	{
+		"bg": "res://assets/textures/scene_6_2.webp",
+		"title": "vila"
+	},
+	{
+		"bg": "res://assets/textures/scene_6_3.webp",
+		"title": "povoado"
+	},
+	{
+		"bg": "res://assets/textures/scene_6_4.webp",
+		"title": "distrito"
+	},
+	{
+		"bg": "res://assets/textures/scene_6_5.webp",
+		"title": "cidade"
+	},
+]
+var time = 0
 
 func _ready() -> void:
 	var new_dialod: DialogScreen = DIALOG_SCREEN.instantiate()
@@ -19,6 +45,16 @@ func _ready() -> void:
 	hud.add_child(new_dialod)
 	
 	new_dialod.connect("dialog_finished", _on_dialog_finished)
+	
+	titleLabel = hud.get_node("Title")
+	bgTexture = hud.get_node("TextureRect")
 
 func _on_dialog_finished() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/menus/main.tscn")
+
+func _on_timer_timeout() -> void:
+	time += 1
+
+	if time < 5:
+		titleLabel.text = "%s PINTADAS" % phases[time]["title"].to_upper()
+		bgTexture.texture = load(phases[time]["bg"])
